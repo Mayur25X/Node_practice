72c2b51465dc64488789dafad77d77454b6bfcc5
Mayur25X <mayuruikey5678@gmail.com>
2022-07-11 23:43:14 +0530

    Todo list Update

--- todo-list_v1/app.js
+++ todo-list_v1/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const BodyParser = require("body-parser");
 const https = require("https");
-
-
 const app = express();
+
+
+app.use(express.static("public"));
+
+var items = ["Buy Food", "Drink Water", "Wacthing Anime"];
+let Itemwork = [];
 
 app.use(BodyParser.urlencoded({ extends: true }));
 app.set('view engine', 'ejs');
@@ -13,46 +17,55 @@ app.get("/", (req, res) => {
 
     var today = new Date();
     // var currentDay = today.getDay();
-    var option={
-        weekday:"long",
-        day:"numeric",
-        month:"long"
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
-    var day = today.toLocaleDateString("en-US",option);
-    // switch (currentDay) {
-    //     case 0:
-    //         day = "Sunday";
-    //         break;
-    //     case 1:
-    //         day = "Monday";
-    //         break;
-    //     case 2:
-    //         day = "Tuesday";
-    //         break;
-    //     case 3:
-    //         day = "wednesday";
-    //         break;
-    //     case 4:
-    //         day = "Thursday";
-    //         break;
-    //     case 5:
-    //         day = "Firday";
-    //         break;
-    //     case 6:
-    //         day = "Saturday";
-    //         break;
 
-    //     default:
-    //         console.log("Error : current day is equal to : " + currentDay)
-            
-    // }
-    res.render("list", { kindOfday: day });
+    var day = today.toLocaleDateString("en-US", option);
+
+    res.render("list", { ListTitle: day, AddList: items });
+
 
 })
 
-app.post("/",(req,res)=>{
-    var item=req.body.Item;
-    console.log(item);
+
+
+app.get("/work", (req, res) => {
+
+    res.render("list", { ListTitle: "Work - Title", AddList: Itemwork })
+
 })
+
+
+app.post("/", (req, res) => {
+
+    let item = req.body.Item;
+    // console.log(req.body);
+
+    if (req.body.list === "Work") {
+
+        Itemwork.push(item);
+        res.redirect("/work");
+
+    }
+    else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+
+// app.post("/work", (req, res) => {
+//     let item = req.body.Item;
+//     work.push(item);
+//     res.redirect("/");
+// })
+
+app.get("/about",(req,res)=>{
+res.render("about");
+})
+
 
 app.listen(3000, () => console.log("server is running at server 3000"));
