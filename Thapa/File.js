63578add9bd46375104a69481eefad8d12ab8cd7
Mayur25X <mayuruikey5678@gmail.com>
2022-08-02 07:02:42 +0530

    New NodeJS tutorials added

---
+++ Thapa/File.js
@@ -0,0 +1,15 @@
+const fs=require("fs");
+// fs.writeFileSync("read.txt","Welcome to Node JS ");
+
+// fs.writeFileSync("read.txt","HElllo")
+
+
+
+// fs.appendFileSync("read.txt", "  lorem uigfdh tgb hhshgs")
+
+// const buf_data=fs.readFileSync("read.txt");
+// console.log(buf_data.toString());
+
+// to rename file
+
+fs.renameSync("read.txt","ReadWrite.txt");
