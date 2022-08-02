4bbd0c52d35b5f6a5d438a3da76de3190efcb3f4
Mayur25X <mayuruikey5678@gmail.com>
2022-08-02 07:09:06 +0530

    Task File

---
+++ Thapa/TaskJS.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+fs.writeFileSync("Bio.txt", `: The term 'git' is not recognized as the name of a csers\MAYUR\Desktop\web devp\NODE\Thmdlet, function, script file, or
+operable program. Check the spelling of the name, or if a path was included, verify that                          mdlet, function, script file, or 
+the path is correct and try again.                        path was included, verify that    
+At line:1 char:1
++ git status
++ ~~~
+    + CategoryInfo          :`);
+
+    
