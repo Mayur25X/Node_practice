c6902292abca814c3641ef397fef4e9a2bd96de6
Mayur25X <mayuruikey5678@gmail.com>
2022-08-02 10:47:25 +0530

    Updated with task

--- Thapa/TaskJS.js
+++ Thapa/TaskJS.js
@@ -1,10 +1,40 @@
 const fs = require("fs");
-fs.writeFileSync("Bio.txt", `: The term 'git' is not recognized as the name of a csers\MAYUR\Desktop\web devp\NODE\Thmdlet, function, script file, or
-operable program. Check the spelling of the name, or if a path was included, verify that                          mdlet, function, script file, or 
-the path is correct and try again.                        path was included, verify that    
-At line:1 char:1
-+ git status
-+ ~~~
-    + CategoryInfo          :`);
 
-    
+
+
+// fs.writeFileSync("Bio.txt", `: The term 'git' is not recognized as the name of a csers\MAYUR\Desktop\web devp\NODE\Thmdlet, function, script file, or
+// operable program. Check the spelling of the name, or if a path was included, verify that                          mdlet, function, script file, or 
+// the path is correct and try again.                        path was included, verify that    
+// At line:1 char:1
+// + git status
+// + ~~~
+//     + CategoryInfo          :`);
+
+
+
+
+
+
+
+//     fs .appendFileSync("Bio.txt",`  + CategoryInfo          : ObjectNotFound: (git:String) [], CommandNotFoundException
+//     + FullyQualifiedErrorId : CommandNotFoundException   
+// `);
+
+
+
+
+
+
+// const buff=fs.readFileSync("MyBio.txt","UTF8");
+// console.log(buff)
+
+
+
+
+
+// fs.renameSync("Bio.txt","MyBio.txt")
+
+ 
+
+
+fs.unlinkSync("MyBio.txt")
