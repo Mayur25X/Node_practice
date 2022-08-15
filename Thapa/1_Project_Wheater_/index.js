d4a00b900cbbbd38478976170b0737f9773dde9e
Mayur25X <mayuruikey5678@gmail.com>
2022-08-15 15:26:48 +0530

    Ejs Learn same hbs

--- Thapa/1_Project_Wheater_/index.js
+++ Thapa/1_Project_Wheater_/index.js
@@ -4,18 +4,18 @@ const http = require("http");
 const reqt = require("requests");
 
 const homeFile = fs.readFileSync("index.html", "utf-8");
-const replaceVal=(tempval,orgval)=>{
-    let temperature=tempval.replace("{%tempval%}",orgval.main.temp);
-    temperature=temperature.replace("{%humidity%}",orgval.main.humidity);
-    temperature=temperature.replace("{%wind_speed%}",orgval.wind.speed);
-    temperature=temperature.replace("{%pressure%}",orgval.main.pressure);
-    temperature=temperature.replace("{%name%}",orgval.name);
-    temperature=temperature.replace("{%icon%}",orgval.weather[0].icon);
+const replaceVal = (tempval, orgval) => {
+    let temperature = tempval.replace("{%tempval%}", orgval.main.temp);
+    temperature = temperature.replace("{%humidity%}", orgval.main.humidity);
+    temperature = temperature.replace("{%wind_speed%}", orgval.wind.speed);
+    temperature = temperature.replace("{%pressure%}", orgval.main.pressure);
+    temperature = temperature.replace("{%name%}", orgval.name);
+    temperature = temperature.replace("{%icon%}", orgval.weather[0].icon);
     console.log(orgval.weather[0].icon);
-    
+
     // console.log(orgval.rain.1h)
     // temperature=temperature.replace("{%rain%}",orgval.rain);
-    temperature=temperature.replace("{%condition%}",orgval.weather[0].description);
+    temperature = temperature.replace("{%condition%}", orgval.weather[0].description);
     return temperature;
 }
 
@@ -24,27 +24,20 @@ const server = http.createServer((req, res) => {
 
 
         reqt('https://api.openweathermap.org/data/2.5/weather?q=Nagpur&units=metric&appid=52e77abe3013fc6f7944d45212e5ec9c')
-            .on('data',function (chunk) {
+            .on('data', function (chunk) {
 
                 const objData = JSON.parse(chunk);
-               const arrObj=[objData];
-
-
-            //    console.log(arrObj);
-            //    console.log(arrObj[0].name);
-
-
-            const realTimeData=arrObj.map((val)=>replaceVal(homeFile,val)).join(" ");
-
-    //  console.log(realTimeData);
-
-    res.write(realTimeData);
-
+                const arrObj = [objData];
+                //    console.log(arrObj);
+                //    console.log(arrObj[0].name);
+                const realTimeData = arrObj.map((val) => replaceVal(homeFile, val)).join(" ");
+                //  console.log(realTimeData);
+                res.write(realTimeData);
             }
             )
-            .on('end',  (err)=> {
+            .on('end', (err) => {
                 if (err) return console.log('connection closed due to errors', err);
-                    res.end();
+                res.end();
                 // console.log('end');
             });
 
---
+++ Thapa/31_HBS_without_views/app.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+
+
+const staticPath=path.join(__dirname+"/public");
+
+/// set Path for Template File
+const templatesPath=path.join(__dirname+"/templates");
+console.log(templatesPath);
+
+
+//set Views as tmplate File
+app.set("views",templatesPath);
+app.set("view engine","hbs");
+
+
+app.use(express.static(staticPath));
+
+
+
+app.get("/hbs",(req,res)=>{
+    res.render("index");
+})
+
+app.listen(2000,()=>{
+    console.log("connected");
+})
+
+
+
+// 1) Medicin dealership
+// --passWord
+// --stock curd
+// ---list of company
+// ---sales
+
+
+// // 2)Kmap
+
+// 2)
